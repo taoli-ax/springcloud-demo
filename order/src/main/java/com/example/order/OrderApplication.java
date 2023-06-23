4fe0811137f8aa6f58ee033cee080f81eea689c2
taoli-ax <deuta-tao@outlook.com>
2023-06-23 12:47:15 +0800

    reset to hystrix theme

--- order/src/main/java/com/example/order/OrderApplication.java
+++ order/src/main/java/com/example/order/OrderApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -19,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class OrderApplication {
     public static void main(String[] args){
         SpringApplication.run(OrderApplication.class,args);
--- products/src/main/java/com/example/products/Application.java
+++ products/src/main/java/com/example/products/Application.java
@@ -3,6 +3,7 @@ package com.example.products;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @MapperScan("com.example.products.mapper")
 @EnableEurekaClient
 @EnableDiscoveryClient
+
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
